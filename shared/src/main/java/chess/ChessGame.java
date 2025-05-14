9d4acb4f68c937c6855a832f86ca88b84b0472f3
Zach <zcs2002@byu.edu>
2025-05-14 09:42:42 -0600

    stalemate works, all tests still pass

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -166,17 +166,16 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-//        if (isInCheck(teamColor)){return false;}
-//        for(int row = 1; row <= 8; row ++){
-//            for (int col = 1; col <= 8; col ++){
-//                ChessPiece piece = board.getPiece(new ChessPosition(row, col));
-//                if(piece == null || piece.getTeamColor() != teamColor){continue;}
-//                Collection<ChessMove> moves = validMoves(new ChessPosition(row, col));
-//                if(!moves.isEmpty()){return false;}
-//            }
-//        }
-//        return true;
-        throw new RuntimeException("Not implemented");
+        if (isInCheck(teamColor)){return false;}
+        for(int row = 1; row <= 8; row ++){
+            for (int col = 1; col <= 8; col ++){
+                ChessPiece piece = board.getPiece(new ChessPosition(row, col));
+                if(piece == null || piece.getTeamColor() != teamColor){continue;}
+                Collection<ChessMove> moves = validMoves(new ChessPosition(row, col));
+                if(!moves.isEmpty()){return false;}
+            }
+        }
+        return true;
     }
 
     /**
