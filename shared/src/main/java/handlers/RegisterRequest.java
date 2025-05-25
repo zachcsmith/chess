a89e30632ede36c3e2bc6409ea079fd236f31748
Zach <zcs2002@byu.edu>
2025-05-25 15:15:21 -0600

    oops i forgot about this, got a bunch of the files made and started register

---
+++ shared/src/main/java/handlers/RegisterRequest.java
@@ -0,0 +1,4 @@
+package handlers;
+
+public record RegisterRequest(String username, String password, String email) {
+}
---
+++ shared/src/main/java/handlers/RegisterResult.java
@@ -0,0 +1,4 @@
+package handlers;
+
+public record RegisterResult(String username, String authToken) {
+}
