54263b993dec8f8fbd92380b09d73b3dd35d7d6c
Zach <zcs2002@byu.edu>
2025-09-30 11:42:27 -0600

    implemented stalemate, all tests are green

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -183,7 +183,11 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if (isInCheck(teamColor)) {
+            return false;
+        } else {
+            return anyPossibleMoves(teamColor);
+        }
     }
 
     /**
