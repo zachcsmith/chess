52ecec2d68e091b51402602d4bc9f19724c8c900
Zach <zcs2002@byu.edu>
2025-10-09 10:31:38 -0600

    implemented a delete and register in Server, does not actually talk to the service or database

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,40 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        server = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", ctx -> register(ctx));
 
+    }
+
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        String reqJson = ctx.body();
+        var req = serializer.fromJson(reqJson, Map.class);
+        //call to the service and register
+
+        var res = Map.of("username", req.get("username"), "authToken", "yzx");
+        ctx.result(serializer.toJson(res));
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
     public void stop() {
-        javalin.stop();
+        server.stop();
     }
 }
