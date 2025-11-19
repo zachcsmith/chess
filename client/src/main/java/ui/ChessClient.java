0f9ee9f383fb6c58406fd00f04c98da155a9e419
Zach <zcs2002@byu.edu>
2025-11-19 13:55:01 -0700

    client can get user input, just echos it rn

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,11 +1,26 @@
 package ui;
 
+import static ui.EscapeSequences.*;
+
+import java.util.Scanner;
+
 public class ChessClient {
     private State state = State.LOGGED_OUT;
     private ServerFacade facade;
+    Scanner scanner = new Scanner(System.in);
 
     public ChessClient(String port) {
         facade = new ServerFacade(port);
+    }
+
+    public void run() {
+        System.out.println("Welcome to Chess: type help to begin.");
+        var response = "";
+        while (!response.equals("quit")) {
+            System.out.print("\n" + RESET_TEXT_COLOR + getState() + ">>> " + SET_TEXT_COLOR_GREEN);
+            String line = scanner.nextLine();
+            System.out.print(line);
+        }
     }
 
     public State getState() {
