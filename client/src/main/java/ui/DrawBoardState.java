d623174553e3a49214da0f2ed12acbf40e97db9a
Zach <zcs2002@byu.edu>
2025-11-19 18:25:45 -0700

    started writing code to draw the chess board

--- client/src/main/java/ui/DrawBoardState.java
+++ client/src/main/java/ui/DrawBoardState.java
@@ -1,4 +1,43 @@
 package ui;
 
+import chess.ChessBoard;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static ui.EscapeSequences.*;
+
 public class DrawBoardState {
+    private boolean whitePerspective;
+    private ChessBoard board;
+    private final PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+    public DrawBoardState(ChessBoard importedBoard, boolean whitePerspective) {
+        this.board = importedBoard;
+        this.whitePerspective = whitePerspective;
+    }
+
+    public void drawBoard() {
+        System.out.print(ERASE_SCREEN);
+        System.out.println();
+        if (whitePerspective) {
+            drawHeader();
+            for (int row = 8; row >= 1; row--) {
+                drawRow(row);
+            }
+            drawHeader();
+        }
+
+    }
+
+    private void drawHeader() {
+
+        if (whitePerspective) {
+            out.print(SET_BG_COLOR_DARK_GREY + SET_TEXT_COLOR_WHITE);
+            out.print("   ");
+        }
+    }
+
+    private void drawRow(int row) {
+    }
 }
