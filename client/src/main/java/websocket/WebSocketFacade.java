6623512b2f1da6304d612f95189086b49707dc9b
Zach <zcs2002@byu.edu>
2025-12-10 13:00:31 -0700

    started working on websocketFacade and made a server message observer based on the petshop

---
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -0,0 +1,36 @@
+package websocket;
+
+import jakarta.websocket.Endpoint;
+import jakarta.websocket.*;
+import ui.ResponseException;
+import websocket.messages.ServerMessage;
+
+import java.net.URI;
+
+public class WebSocketFacade extends Endpoint {
+    private Session session;
+    ServerMessageObserver serverMessageObserver;
+
+    public WebSocketFacade(String url, ServerMessageObserver observer) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.serverMessageObserver = observer;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    Notification notification = new Gson().fromJson(message, Notification.class);
+                    notificationHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+}
